d0a22ffe35d4c5d57d2d2df129c67b2f5e61d61f
Akshat <iamakku0.0.0.1@gmail.com>
2023-06-23 23:18:12 +0530

    commiting

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,7 +1,7 @@
 //importing cloudinary
 const cloudinary = require("cloudinary").v2;
 
-exports.cloudinary.connect = ()=>{
+exports.cloudinaryConnect = () => {
     try{
         //using config method of cloudinary to connect with server 
         //we have to pass 3 things in config method
@@ -10,6 +10,7 @@ exports.cloudinary.connect = ()=>{
             api_key : process.env.API_KEY,
             api_secret: process.env.API_SECRET
         })
+        console.log("cloudinary connected")
     }
     catch(err){
         console.log(err)
--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const server = express();
 require("dotenv").config();
 const port = process.env.PORT;
 
+//middleware to parse the json object
 server.use(express.json());
+
+//express-fileupload is a middleware to upload files to server into temp file and then upload it to media server which is cloudinary and delete file from server
+// because express can only interact with js objects, so we need to parse the file
+const fileUploadMiddleware = require("express-fileupload");
+server.use(fileUploadMiddleware())
 
 const mediaRoutes = require("./routes/mainRoutes");
 server.use("/api/v1",mediaRoutes);
+
+
+
+const db_connect  = require("./config/db");
+ db_connect()
+
+const cloudinary = require("./config/cloudinary")
+cloudinary.cloudinaryConnect()
 
 server.listen(port , ()=>{
     console.log("server is ON");
 })
 
-const db_connect  = require("./config/db")
-db_connect();
